1fb976b2df7804ef1b78ab905f31837b05938d30
Brent Zurek <zburns1980@gmail.com>
2024-06-21 11:08:56 -0600

    small update to vite config and updates npm packages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const apiUrl = mode === 'development' ? 'http://localhost:3000' : '';
+  const apiUrl = mode === 'development' ? 'http://127.0.0.1:3000' : '';
 
   return {
     plugins: [react()],
