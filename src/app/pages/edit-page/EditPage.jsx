2ecdf9175d3c2436c17d28d5149dff2df9b84133
Brent Zurek <zburns1980@gmail.com>
2024-11-09 14:18:44 -0700

    small change

--- src/app/pages/edit-page/EditPage.jsx
+++ src/app/pages/edit-page/EditPage.jsx
@@ -3,14 +3,21 @@ import { useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { PageWrapper } from '../';
+import { DataEntryForm } from '../../containers';
 import { Error, LoadSpinner } from '../../components';
+import useRouteForm from '../add-new-route/useRouteForm';
 
 const EditPage = (props) => {
+  const { defaultFormFields, dynamicProps } = useRouteForm();
   const { routeId } = useParams();
 
   return (
     <PageWrapper title={`Edit ${routeId}`}>
-      <div>EditPage</div>
+      <DataEntryForm
+        dataTc="editRouteForm"
+        fields={defaultFormFields}
+        dynamicProps={dynamicProps}
+      />
     </PageWrapper>
   );
 };
